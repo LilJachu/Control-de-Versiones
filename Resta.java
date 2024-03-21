9df7e7f98c78fc7f495b292376bc9fafe921fdd7
LilJachu <129267407+LilJachu@users.noreply.github.com>
2024-03-21 12:17:47 +0100

    Update Resta.java
    3 instantáneas realizadas

--- Resta.java
+++ Resta.java
@@ -2,12 +2,12 @@ package Git;
 
 public class Resta {
 	private double respuesta;
-	private double a;
-	private double b;
+	private double num1;
+	private double num2;
 	
 	public double resta() {
-		double resta = a-b;
-		answer = resta;
+		double resta = num1-num2;
+		respuesta = resta;
 		return respuesta;
 	}
 }
