b94a076e024cbd74a15a98690137d77931a55a90
LilJachu <129267407+LilJachu@users.noreply.github.com>
2024-03-21 12:06:36 +0100

    Updated Resta.java
    Nombre de variable "Answer" cambiado

--- Resta.java
+++ Resta.java
@@ -1,13 +1,13 @@
 package Git;
 
 public class Resta {
-	private double answer;
+	private double respuesta;
 	private double a;
 	private double b;
 	
 	public double resta() {
 		double resta = a-b;
 		answer = resta;
-		return answer;
+		return respuesta;
 	}
 }
